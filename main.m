f3c0ba9900924017befbf6dfe1c6d1cfce93cb55
ziyimo <zm561@nyu.edu>
2019-02-28 16:47:30 -0500

    Created a bash job script to run the MATLAB script on UGE cluster

--- main.m
+++ main.m
@@ -5,13 +5,13 @@ clear ; close all; clc
 % weights, biases, variables and errors are all stored in cell arrays.
 
 params.act = 'tanh'; %activation function: 'tanh', 'sig', 'lin', 'reclin'
-params.l_rate =  1; % learning rate
+params.l_rate =  0.5; % learning rate
 params.it_max = 100; % maximum iterations of inference
-params.epochs = 200; % number of epochs
+params.epochs = 500; % number of epochs
 params.d_rate = 0; % weight decay parameter
 params.beta = 0.1; % euler integration constant
 
-params.layer_sizes = [4, 5, 3, 3]; %units in each layer
+params.layer_sizes = [2, 5, 2, 2]; %units in each layer
 % note: last two layers together constitute output layer, their size must
 % equal!
 params.n_layers = length(params.layer_sizes); % number of layers
@@ -20,11 +20,11 @@ var(end)=1; % variance on last layer
 params.var=var;
 
 % XOR problem
-% sin = [0 0 1 1;
-%        0 1 0 1];
-% y = [1, 2, 2, 1]';
-% sout = [1 0 0 1;
-%         0 1 1 0];
+sin = [0 0 1 1;
+        0 1 0 1];
+y = [1, 2, 2, 1]';
+sout = [1 0 0 1;
+         0 1 1 0];
 
 % MNIST
 %load('mnist_5000.mat');
@@ -33,25 +33,27 @@ params.var=var;
 %sout = bsxfun(@eq, 1:params.layer_sizes(end), y(1:500))'; % Apply element-wise operation to two arrays with implicit expansion enabled
 
 % Iris dataset
-load('fisheriris.mat');
-sin = meas';
-keySet = {'setosa', 'versicolor', 'virginica'};
-valueSet = [1, 2, 3];
-M = containers.Map(keySet,valueSet);
-y = cellfun(@(x) M(x), species);
-sout = double(bsxfun(@eq, 1:3, y))';
+%load('fisheriris.mat');
+%sin = meas';
+%keySet = {'setosa', 'versicolor', 'virginica'};
+%valueSet = [1, 2, 3];
+%M = containers.Map(keySet,valueSet);
+%y = cellfun(@(x) M(x), species);
+%sout = double(bsxfun(@eq, 1:3, y))';
 
 [w_pc, b_pc] = rand_init(params); % get weights and biases parameters
 
+log_f = fopen(strcat(datestr(now, 'mmdd_HHMMSS'), '.txt'), 'w');
 %learn
 for epoch = 1:params.epochs
     params.epoch_num = epoch;
     [w_pc,b_pc] = learn_pc(sin,sout,w_pc,b_pc,params); %train pc
     [~, pc_out] = predict(sin, w_pc, b_pc, params); % examine cost
-    J = -sum(log(pc_out).*sout, 'all')/length(sin);
-    fprintf('Epoch %d | Cost: %e\r', epoch, J);
+    J = -sum(sum(log(pc_out).*sout))/length(sin);
+    fprintf(log_f, 'Epoch %d | Cost: %e\n', epoch, J);
 end
 
 [y_pred, O] = predict(sin, w_pc, b_pc, params);
 
-fprintf('\nTraining Set Accuracy: %f\n', mean(double(y_pred == y')) * 100);
+fprintf(log_f, '\nTraining Set Accuracy: %f\n', mean(double(y_pred == y')) * 100);
+fclose(log_f);
